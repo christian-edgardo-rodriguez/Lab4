c22f9db88229e36b64826c0b3c48381a0b7fafc3
christian-edgardo-rodriguez <christian.rodriguez@unitec.edu>
2016-02-12 17:26:43 -0600

    hice los ataques normales, los waves, y las validaciones de waves y expansions

--- lab4.cpp
+++ lab4.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -14,6 +15,7 @@ int menu(int, int);
 
 int main(int argc, char const *argv[]){
 	srand(time(NULL));
+	int x1,y1,z1,x2,y2, z2;
 	int size=12, bandera=1;
 	int cantCoor = 15, wave1=3, expansive1=3, wave2=3, expansive2=3, cantJUG1 = 14, cantJUG2 = 14;
 	bool win=false;
@@ -29,20 +31,99 @@ int main(int argc, char const *argv[]){
 			cout << "NUMERO DE SUBMARINOS ENEMIGOS" << cantJUG2 << endl;
 			int opcion1=menu(wave1, expansive1);
 			if (opcion1 == 1){
-				cout << "Ataque1" << endl;
+				cout << "x: ";
+				cin>>x1;
+				cout << "y: ";
+				cin>>y1;
+				cout << "z: ";
+				cin>>z1;
+				if (arregloJug2[x1][y1][z1]==1){
+					cout<<"("<<x1<<", "<<y1<<", "<<z1<<") ha sido destruido. "<<endl;
+					cantJUG2--;
+					arregloJug2[x1][y1][z1]=0;
+					bandera=2;
+				}else
+					cout<<"No destrui nada"<<endl;
+					bandera=2;
 			}else if (opcion1 == 2){
-				cout << "Wave Attack XY" << endl;
+				while(wave1!=0){
+					cout<<"z: ";
+					cin>>z1;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug2[i][j][z1]==1){
+								cout<<"("<<i<<", "<<j<<", "<<z1<<") ha sido destruido. "<<endl;
+								cantJUG2--;
+								arregloJug2[i][j][z1]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave1--;
+					bandera=2;
+					break;
+				}
+				if (wave1==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=2;
+				}
 			}else if (opcion1 == 3){
-				cout << "wave Attack ZY" << endl;
+				while(wave1!=0){
+					cout<<"y: ";
+					cin>>y1;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug2[i][y1][j]==1){
+								cout<<"("<<i<<", "<<y1<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG2--;
+								arregloJug2[i][y1][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave1--;
+					bandera=2;
+					break;
+				}
+				if (wave1==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=2;
+				}	
 			}else if(opcion1 == 4){
-				cout << "wave Attack ZX" << endl;
+				while(wave1!=0){
+					cout<<"x: ";
+					cin>>x1;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug2[x1][i][j]==1){
+								cout<<"("<<x1<<", "<<i<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG2--;
+								arregloJug2[x1][i][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave1--;
+					bandera=2;
+					break;
+				}
+				if (wave1==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=2;
+				}
 			}else if (opcion1 == 5){
-				cout << "expansive Attack" << endl;
+				while(expansive1!=0){
+
+				}
+				if (expansive1==0){
+					cout<<"Ya no tiene mas expansives."<<endl;
+					bandera=2;
+				}
 			}else{
 				cout <<"No es una opcion -_-!!!" << endl;
 			}
 			if (cantJUG2==0){
-				cout << "Ganastes!!!" << endl;
+				cout << "Ganaste!!!" << endl;
 				win = true;
 				break;
 			}
@@ -54,15 +135,94 @@ int main(int argc, char const *argv[]){
 			cout << "NUMERO DE SUBMARINOS ENEMIGOS" << cantJUG2 << endl;
 			int opcion2=menu(wave2, expansive2);
 			if (opcion2 == 1){
-				cout << "Ataque1" << endl;
+				cout << "x: " ;
+				cin>>x2;
+				cout << "y: " ;
+				cin>>y2;
+				cout << "z: " ;
+				cin>>z2;
+				if (arregloJug1[x2][y2][z2]==1){
+					cout<<"("<<x2<<", "<<y2<<", "<<z2<<") ha sido destruido. "<<endl;
+					cantJUG1--;
+					arregloJug1[x2][y2][z2]=0;
+					bandera=1;
+				}else
+					cout<<"No destrui nada"<<endl;
+					bandera=1;
 			}else if (opcion2 == 2){
-				cout << "Wave Attack XY" << endl;
+				while(wave2!=0){
+					cout<<"z: ";
+					cin>>z2;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug1[i][j][z2]==1){
+								cout<<"("<<i<<", "<<j<<", "<<z2<<") ha sido destruido. "<<endl;
+								cantJUG1--;
+								arregloJug1[i][j][z2]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave2--;
+					bandera=1;
+					break;
+				}
+				if (wave2==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=1;
+				}	
 			}else if (opcion2 == 3){
-				cout << "wave Attack ZY" << endl;
+				while(wave2!=0){
+					cout<<"y: ";
+					cin>>y2;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug1[i][y2][j]==1){
+								cout<<"("<<i<<", "<<y2<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG1--;
+								arregloJug1[i][y2][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave2--;
+					bandera=1;
+					break;
+				}
+				if (wave2==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=1;
+				}
 			}else if(opcion2 == 4){
-				cout << "wave Attack ZX" << endl;
+				while(wave2!=0){
+					cout<<"x: ";
+					cin>>x2;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug1[x2][i][j]==1){
+								cout<<"("<<x2<<", "<<i<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG1--;
+								arregloJug1[x1][i][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave2--;
+					bandera=1;
+					break;
+				}
+				if (wave2==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=1;
+				}
 			}else if (opcion2 == 5){
-				cout << "expansive Attack" << endl;
+				while(expansive2!=0){
+
+				}
+				if (expansive2==0){
+					cout<<"Ya no tiene mas expansives."<<endl;
+					bandera=1;
+				}
 			}else{
 				cout <<"No es una opcion -_-!!!" << endl;
 			}
--- pruebalab4.cpp
+++ pruebalab4.cpp
@@ -8,32 +8,235 @@ using std::endl;
 
 int***newArray();
 void deletePunteros(int***,int);
-void randomLlenar(int***, int, int);
+void randomLlenar1(int***, int, int);
+void randomLlenar2(int***, int, int);
 void imprimirMatriz(int***, int);
 int menu(int, int);
 
 int main(int argc, char const *argv[]){
 	srand(time(NULL));
+	int x1,y1,z1,x2,y2, z2;
 	int size=12, bandera=1;
-	int cantCoor = 15, wave1=3, expansive1=3, wave2=3, expansive2=3;
+	int cantCoor = 15, wave1=3, expansive1=3, wave2=3, expansive2=3, cantJUG1 = 14, cantJUG2 = 14;
 	bool win=false;
 	int***arregloJug1=newArray();
 	int***arregloJug2=newArray();
-	randomLlenar(arregloJug1, size, cantCoor);
-	randomLlenar(arregloJug2, size, cantCoor);
+	randomLlenar1(arregloJug1, size, cantCoor);
+	randomLlenar2(arregloJug2, size, cantCoor);
 	do{
 		if (bandera==1){
 			cout<<"---------------MAPA DE SUBMARINOS DEL JUGADOR 1-----------------"<<endl;
 			imprimirMatriz(arregloJug1, size);
 			cout<<"-----------MAPA DE ATAQUES-------------"<<endl;
+			cout << "NUMERO DE SUBMARINOS ENEMIGOS" << cantJUG2 << endl;
 			int opcion1=menu(wave1, expansive1);
+			if (opcion1 == 1){
+				cout << "x: ";
+				cin>>x1;
+				cout << "y: ";
+				cin>>y1;
+				cout << "z: ";
+				cin>>z1;
+				if (arregloJug2[x1][y1][z1]==1){
+					cout<<"("<<x1<<", "<<y1<<", "<<z1<<") ha sido destruido. "<<endl;
+					cantJUG2--;
+					arregloJug2[x1][y1][z1]=0;
+					bandera=2;
+				}else
+					cout<<"No destrui nada"<<endl;
+					bandera=2;
+			}else if (opcion1 == 2){
+				while(wave1!=0){
+					cout<<"z: ";
+					cin>>z1;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug2[i][j][z1]==1){
+								cout<<"("<<i<<", "<<j<<", "<<z1<<") ha sido destruido. "<<endl;
+								cantJUG2--;
+								arregloJug2[i][j][z1]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave1--;
+					bandera=2;
+					break;
+				}
+				if (wave1==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=2;
+				}
+			}else if (opcion1 == 3){
+				while(wave1!=0){
+					cout<<"y: ";
+					cin>>y1;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug2[i][y1][j]==1){
+								cout<<"("<<i<<", "<<y1<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG2--;
+								arregloJug2[i][y1][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave1--;
+					bandera=2;
+					break;
+				}
+				if (wave1==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=2;
+				}	
+			}else if(opcion1 == 4){
+				while(wave1!=0){
+					cout<<"x: ";
+					cin>>x1;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug2[x1][i][j]==1){
+								cout<<"("<<x1<<", "<<i<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG2--;
+								arregloJug2[x1][i][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave1--;
+					bandera=2;
+					break;
+				}
+				if (wave1==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=2;
+				}
+			}else if (opcion1 == 5){
+				while(expansive1!=0){
+
+				}
+				if (expansive1==0){
+					cout<<"Ya no tiene mas expansives."<<endl;
+					bandera=2;
+				}
+			}else{
+				cout <<"No es una opcion -_-!!!" << endl;
+			}
+			if (cantJUG2==0){
+				cout << "Ganaste!!!" << endl;
+				win = true;
+				break;
+			}
+			bandera=2;
 		}else if(bandera==2){
 			cout<<"---------------MAPA DE SUBMARINOS DEL JUGADOR 2-----------------"<<endl;
 			imprimirMatriz(arregloJug2, size);
 			cout<<"-----------MAPA DE ATAQUES-------------"<<endl;
+			cout << "NUMERO DE SUBMARINOS ENEMIGOS" << cantJUG2 << endl;
 			int opcion2=menu(wave2, expansive2);
+			if (opcion2 == 1){
+				cout << "x: " ;
+				cin>>x2;
+				cout << "y: " ;
+				cin>>y2;
+				cout << "z: " ;
+				cin>>z2;
+				if (arregloJug1[x2][y2][z2]==1){
+					cout<<"("<<x2<<", "<<y2<<", "<<z2<<") ha sido destruido. "<<endl;
+					cantJUG1--;
+					arregloJug1[x2][y2][z2]=0;
+					bandera=1;
+				}else
+					cout<<"No destrui nada"<<endl;
+					bandera=1;
+			}else if (opcion2 == 2){
+				while(wave2!=0){
+					cout<<"z: ";
+					cin>>z2;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug1[i][j][z2]==1){
+								cout<<"("<<i<<", "<<j<<", "<<z2<<") ha sido destruido. "<<endl;
+								cantJUG1--;
+								arregloJug1[i][j][z2]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave2--;
+					bandera=1;
+					break;
+				}
+				if (wave2==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=1;
+				}	
+			}else if (opcion2 == 3){
+				while(wave2!=0){
+					cout<<"y: ";
+					cin>>y2;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug1[i][y2][j]==1){
+								cout<<"("<<i<<", "<<y2<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG1--;
+								arregloJug1[i][y2][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave2--;
+					bandera=1;
+					break;
+				}
+				if (wave2==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=1;
+				}
+			}else if(opcion2 == 4){
+				while(wave2!=0){
+					cout<<"x: ";
+					cin>>x2;
+					for (int i = 0; i < size; ++i){
+						for (int j = 0; j < size; ++j){
+							if (arregloJug1[x2][i][j]==1){
+								cout<<"("<<x2<<", "<<i<<", "<<j<<") ha sido destruido. "<<endl;
+								cantJUG1--;
+								arregloJug1[x1][i][j]=0;
+							}else
+								cout<<"No destrui nada"<<endl;
+						}
+					}
+					wave2--;
+					bandera=1;
+					break;
+				}
+				if (wave2==0){
+					cout<<"Ya no tiene mas waves."<<endl;
+					bandera=1;
+				}
+			}else if (opcion2 == 5){
+				while(expansive2!=0){
+
+				}
+				if (expansive2==0){
+					cout<<"Ya no tiene mas expansives."<<endl;
+					bandera=1;
+				}
+			}else{
+				cout <<"No es una opcion -_-!!!" << endl;
+			}
+			if (cantJUG1==0){
+				cout << "Ganastes!!!" << endl;
+				win = true;
+				break;
+			}
+			bandera=1;
 		}
 	}while(win==false);
+	imprimirMatriz(arregloJug1, size);
+	cout<<"--------------------------------"<<endl;
+	imprimirMatriz(arregloJug2, size);
 	deletePunteros(arregloJug1, size);
 	deletePunteros(arregloJug2 ,size);
 	return 0;
@@ -65,7 +268,8 @@ void deletePunteros(int*** arreglo, int size){
 	}
 	delete[] arreglo;//fin delete
 }
-void randomLlenar(int*** array, int size, int cantCoor){
+void randomLlenar1(int*** array, int size, int cantCoor){
+	bool validCoor;
 	for (int i = 0; i < size; ++i){
 		for (int j = 0; j < size; ++j){
 				for (int k = 0; k < size; ++k){
@@ -74,10 +278,42 @@ void randomLlenar(int*** array, int size, int cantCoor){
 			}	
 	}
 	for (int i = 0; i < cantCoor; ++i){
-		int coordX=rand()%12;
-		int coordY=rand()%12;
-		int coordZ=rand()%12;
-		array[coordX][coordY][coordZ] = 1;
+		while(validCoor){
+			int coordX=rand()%12;
+			int coordY=rand()%12;
+			int coordZ=rand()%12;
+			if (array[coordX][coordY][coordZ] == 1){
+				validCoor = true;
+			}else{
+				array[coordX][coordY][coordZ] = 1;
+				validCoor = false;
+			}
+	    }
+	    validCoor= true;
+	}
+}
+void randomLlenar2(int*** array, int size, int cantCoor){
+	bool validCoor;
+	for (int i = 0; i < size; ++i){
+		for (int j = 0; j < size; ++j){
+				for (int k = 0; k < size; ++k){
+						array[i][j][k]=0;
+					}	
+			}	
+	}
+	for (int i = 0; i < cantCoor; ++i){
+		while(validCoor){
+			int coordX=rand()%12;
+			int coordY=rand()%12;
+			int coordZ=rand()%12;
+			if (array[coordX][coordY][coordZ] == 1){
+				validCoor = true;
+			}else{
+				array[coordX][coordY][coordZ] = 1;
+				validCoor = false;
+			}
+	    }
+	    validCoor= true;
 	}
 }
 void imprimirMatriz(int*** array, int size){
