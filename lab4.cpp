40a108d52880eccd7dfd2498e0d4ef596fca6711
juanoh <juanoh@unitec.edu>
2016-02-12 13:59:55 -0600

    delete punteros

--- lab4.cpp
+++ lab4.cpp
@@ -11,9 +11,21 @@ int main(int argc, char const *argv[]){
 		arreglo[i]=new int*[12];
 	}
 	for (int i = 0; i < size; ++i){
-		for (int i = 0; i < size; ++i){
+		for (int j = 0; j < size; ++j){
 			arreglo[i][j]= new int[12];
 		}
+	}//fin new
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < size; ++j)
+		{
+			delete[] arreglo[i][j];
+		}
 	}
+	for (int i = 0; i < size; ++i)
+	{
+		delete[] arreglo[i];
+	}
+	delete[] arreglo;//fin delete
 	return 0;
 }
